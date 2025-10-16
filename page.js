7455567537fb0a209f4f4e282cf66b3d1b731c80
Devp139 <devkumarmanojbhai.chhatrala@edu.sait.ca>
2025-10-16 01:34:32 -0600

    submit

---
+++ page.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">CPRG 306 Assignments</h1>
+      <ul className="list-disc ml-6 space-y-2">
+        <li><Link href="/week-1">Week 1</Link></li>
+        <li><Link href="/week-2">Week 2</Link></li>
+        <li><Link href="/week-3">Week 3</Link></li>
+        <li><Link href="/week-4">Week 4</Link></li>
+        <li><Link href="/week-5">Week 5</Link></li>
+        <li><Link href="/week-6">Week 6</Link></li>
+      </ul>
+    </main>
+  );
+}
+
